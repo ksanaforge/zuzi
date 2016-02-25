f6256ccd49f0345781e8bce8837313234fa2e8e1
yap cheah shen <yapcheahshen@gmail.com>
2016-02-26 00:12:25 +0800

    port from web version

--- src/zhaozimain.js
+++ src/zhaozimain.js
@@ -6,11 +6,12 @@ import React, {
   Text,
   View
 } from 'react-native';
+var {RTIREView}=require("ksana-ire");
 
 var ZhaoziMain=React.createClass({
 	render:function(){
-		return <View style={{top:22}}><Text>FIND CHAR</Text>
-
+		return <View style={{top:22}}>
+			<RTIREView ire="颱台風" height={256}/>
 		</View>
 	}
 })
